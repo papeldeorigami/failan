f2a65201eafba9a74fab5724de7038a6fe5d6c98
papeldeorigami <papeldeorigami@googlemail.com>
2014-05-12 23:36:12 -0300

    use angularjs service for analysis cases

--- webapp/app/scripts/controllers/case-list.js
+++ webapp/app/scripts/controllers/case-list.js
@@ -1,16 +1,6 @@
 'use strict';
 
 angular.module('failanApp')
-  .controller('CaseListCtrl', function ($scope, $location, $log, CaseService) {
-        $scope.cases = CaseService.listAll();
-      
-        $scope.changeView = function(view){
-            $location.path(view); // path not hash
-        }
-        
-        $scope.selectCase = function(selectedCase){
-            $scope.selectedCase = selectedCase;
-            $log.info('selected case ' + selectedCase.casenumber);
-            $scope.changeView('/cases/' + selectedCase.id);
-        }
+  .controller('CaseListCtrl', function ($scope, CaseService) {
+        $scope.cases = CaseService.listAll();      
     });
